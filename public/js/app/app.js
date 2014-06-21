fb6e86bb0963f25cf6eaea32cff7700d5eab0947
thomas <thomas.schiela@gmail.com>
2014-06-21 15:30:24 +0200

    added google maps

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,9 +1,35 @@
 'use strict';
 
-var gardnrApp = angular.module('gardnr-app', []);
+var gardnrApp = angular.module('gardnr-app', [
+  'geo'
+]);
 
 gardnrApp.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.test = "HELLO ANGULAR WORKS";
+  $scope.startingLocation = {
+    lat: 52.513480,
+    lng: 13.393530
+  }
+
+  $scope.markers = [
+    {
+      name: "Power Primeln",
+      address: {
+        location: [13.393530, 52.513480]
+      }
+    },
+    {
+      name: "Power Primeln",
+      address: {
+        location: [13.453530, 52.23480]
+      }
+    },
+    {
+      name: "Power Primeln",
+      address: {
+        location: [13.43530, 52.73480]
+      }
+    }
+  ];
 }]);
 
 
